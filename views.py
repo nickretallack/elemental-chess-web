fb468ee5945d8904cb117acc4607ad72cebb5419
Nick Retallack <nickretallack@gmail.com>
2008-12-07 03:16:45 -0800

    check-in before major changes

--- views.py
+++ views.py
@@ -7,37 +7,49 @@ class Piece(object):
   def __init__(self,options):
     self.rank = options['rank']
     self.kind = options['kind']
-    
+    self.player = options['player']
+    self.x, self.y = options['location']
+
+  def tag(self):
+    return "%s-%s%d" % (self.player, self.kind, self.rank)
+
+  def __str__(self):
+    return tag()
 
 class Target(object):
-  pass
+  def __init__(self,piece,x,y):
+    self.piece = piece
+    self.x = x
+    self.y = y
 
+  def __str__(self):
+    return "%s|%s|%s" % (piece.tag(),x,y)
 
 class Game(object):
   def __init__(self):
     # retrieve game state
     game = couch_request('game',200)
-    self.pieces = game['pieces']
+    #self.pieces = game['pieces']
     self.width = 5
     self.height = 6
 
-    # set up board view
+    # build some local indexes
     self.spaces = [[{} for x in xrange(self.width)] for y in xrange(self.height)]
     for piece_opts in game['pieces']:
       piece = Piece(piece_opts)
-      x,y = game['pieces'][piece]['location']
-      self.spaces[y][x] = {'piece':piece}
+      self.spaces[piece.y][piece.x]['piece'] = piece
+      self.pieces[piece.tag()] = piece
 
   def mark_moves(self,piece_tag):
     piece = self.pieces[piece_tag]
-    x,y = piece['location']
+    x,y = piece.x, piece.y
     for move in [(x+1,y),(x,y+1),(x-1,y),(x,y-1)]:
       nx,ny = move
       if nx >= 0 and nx < self.width and ny >= 0 and ny < self.height: # make sure move lands on the board
         target = self.spaces[ny][nx]
         # unsafe wording....
         if not 'piece' in target or self.battle(piece_tag,target['piece']): # see if move is legal
-          self.spaces[ny][nx]['target'] = piece
+          self.spaces[ny][nx]['target'] = Target(piece)
     
   def battle(self,attacker_tag,defender_tag):
     attacker = self.pieces[attacker_tag]
